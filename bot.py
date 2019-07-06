df2136fcac79612defd8524a77600ae603f822aa
Lauro <laurobmb@gmail.com>
2019-07-06 12:05:48 -0300

    Remoção do TOKEN do código

--- bot.py
+++ bot.py
@@ -519,7 +519,7 @@ def chatterbot_msg(MSG):
 	return output
 
 def main():
-		TOKEN='326030510:AAHHYyZIx_igNjHodRbF4qnnCCRzSSewjOc'
+		TOKEN='XXXXXXXXXXXXXX'
 		updater = Updater(TOKEN, use_context=True)
 		updater.dispatcher.add_handler(CommandHandler('start', start))
 #### Comandos COMUNS
